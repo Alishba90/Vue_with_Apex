5f04cdc45b1b44b2716bb457e68406c7a383d79d
Alishba90 <alishbaarshad90@gmail.com>
2022-03-14 20:09:25 +0500

    side panel

--- src/components/MainTemplate.vue
+++ src/components/MainTemplate.vue
@@ -4,19 +4,23 @@
 
   </div>
   <MainHeader/>
-  <GraphArea/>
+<div style="display:flex">
+  <GraphArea style="position:absolute;width:72%"/>
+  <SidePanel style="position:absolute;right:1em;"/>
+</div>
 </template>
 
 <script>
 import MainHeader from "./subcomponents/MainHeader.vue"
 import GraphArea from "./subcomponents/GraphArea.vue"
+import SidePanel from "./subcomponents/SidePanel.vue"
 export default {
   name: 'MainTemplate',
   props: {
     msg: String
   },
   components:{
-  MainHeader, GraphArea
+  MainHeader, GraphArea,SidePanel
 }
 }
 </script>
--- src/components/subcomponents/SidePanel.vue
+++ src/components/subcomponents/SidePanel.vue
@@ -1,5 +1,12 @@
 <template>
-  
+  <div id="container">
+  <div id="head">
+    <button>Time Series</button>
+    </div>
+    <div id="panel">
+
+    </div>
+</div>
 </template>
 
 <script>
@@ -8,6 +15,45 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+#container{
+width: 25vw;
+height: 85vh;
+overflow: scroll;
+margin-bottom: 2em;
+overflow-x: hidden;
+
+}
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 5px black; 
+  border-radius: 10px;
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background:grey; 
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: grey;
+overflow-y: visible; 
+}
+#panel{
+width: 100%;
+height: 100%;
+background-color: rgb(27, 27, 27);
+scroll-behavior: smooth;
+}
+#head button{
+width: 100%;
+height: 100%;
+}
 
 </style>
